f960eed3e37b0a74ab281030d6f4b9e2be570872
beknazar <beknazar23@gmail.com>
2015-11-29 21:40:58 +0900

    debug false

--- tentechio/tentechio/settings.py
+++ tentechio/tentechio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's52+euk2(()5@kz(vwmro7i@wml84v3cz^juq%)8j$yxcwji-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
